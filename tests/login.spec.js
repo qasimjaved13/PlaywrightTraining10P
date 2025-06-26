576228639d443461e6859a317299707e60c228d3
qasimjaved13 <qasimjaved13@gmail.com>
2025-06-26 13:44:09 +0500

    Update test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -7,7 +7,7 @@ const data = require('../TestData/testData.json')
 test('Login Test', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/')
   await page.locator('#user-name').fill('standard_user')
-  await page.locator('#password').fill('secret_saucewww')
+  await page.locator('#password').fill('secret_sauce')
   await page.locator('#login-button').click()
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 })
